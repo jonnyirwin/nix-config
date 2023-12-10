bee4e853e0656b9feeca62e6ade67f990bda3707
Jonny Irwin <git@jbi.im>
2023-12-10 21:54:27 +0000

    updating sway config

--- home-manager/wm/sway.nix
+++ home-manager/wm/sway.nix
@@ -1,9 +1,12 @@
 { config, pkgs, lib, ... } :
 {
-  wayland.windowManager.sway = {
+  wayland.windowManager.sway = let
+		modifier = "Mod4";
+	in
+	{
 		enable = true;
 		config = {
-		modifier = "Mod4";
+		inherit modifier;
 		window = {
 		  titlebar = false;
 			hideEdgeBorders = "none";
@@ -29,11 +32,30 @@
 		};
 		terminal = "${pkgs.kitty}/bin/kitty";
 		bars = [
-			{ command = "${config.programs.waybar.package}/bin/waybar"; }
+			{ 
+			  command = "${config.programs.waybar.package}/bin/waybar";
+				position = "bottom";
+			}
 		];
 		fonts = {
 		  names = [ "Inter" "NerdFontsSymbolsOnly" ];
 			size = 14.0;
+		};
+		gaps = {
+      bottom = 5;
+			horizontal = 5;
+			inner = 5;
+			left = 5;
+			outer = 5;
+			right = 5;
+			top = 5;
+			vertical = 5;
+			smartGaps = true;
+		};
+		keybindings = lib.mkOptionDefault {
+			"${modifier}+Return" = "exec ${pkgs.kitty}/bin/kitty";
+			"${modifier}+Shift+q" = "kill";
+			"${modifier}+d" = "exec ${pkgs.dmenu}/bin/dmenu_path | ${pkgs.dmenu}/bin/dmenu | ${pkgs.findutils}/bin/xargs swaymsg exec --";
 		};
 		};
 	};

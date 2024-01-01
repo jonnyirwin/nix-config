7cbdb028a2a47454ab26cfb067fb52000a87bb74
Jonny Irwin <git@jbi.im>
2024-01-01 16:08:49 +0000

    nvim-cmp settings

--- home-manager/nvim/default.nix
+++ home-manager/nvim/default.nix
@@ -37,11 +37,52 @@
 			fugitive.enable = true;
 			gitsigns.enable = true;
 			harpoon.enable = true;
-			lsp.enable = true;
+
+			lsp = {
+				enable = true;
+				servers = {
+					rnix-lsp.enable = true;
+				};
+			};
+
 			lspkind.enable = true;
 			lualine.enable = true;
 			luasnip.enable = true;
-			nvim-cmp.enable = true;
+
+			nvim-cmp = {
+				enable = true;
+				sources = [
+					{ name = "nvim_lsp"; }
+					{ name = "luasnip"; }
+					{ name = "buffer"; }
+					{ name = "path";  }
+				];
+				formatting.fields = [ "kind" "abbr" "menu" ];
+				mapping = {
+					"<C-b>" = "cmp.mapping.scroll_docs(-4)";
+					"<C-f>" = "cmp.mapping.scroll_docs(4)";
+					"<C-Space>" = "cmp.mapping.complete()";
+					"<C-e>" = "cmp.mapping.abort()";
+					"<CR>" = "cmp.mapping.confirm({ select = true })";
+				};
+				window.completion = {
+					winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None";
+					colOffset = -4;
+					sidePadding = 0;
+					border = "single";
+				};
+				window.documentation = {
+					winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None";
+					border = "single";
+				};
+        snippet.expand = "luasnip";
+			};
+
+			cmp-buffer.enable = true;
+			cmp-nvim-lsp.enable = true;
+			cmp-path.enable = true;
+			cmp_luasnip.enable = true;
+
 			surround.enable = true;
 			telescope.enable = true;
 			tmux-navigator.enable = true;

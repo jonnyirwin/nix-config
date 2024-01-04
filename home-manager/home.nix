3ab9bc0e15f6aeb7337c0601dc4bc64e4fd13fb8
Jonny Irwin <git@jbi.im>
2024-01-04 21:19:40 +0000

    removed unnecessary default code

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -15,8 +15,6 @@
     ./nvim
   ];
 
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home = {
 		username = "jonny";
 		homeDirectory = "/home/jonny";
@@ -43,17 +41,8 @@
     })
   ];
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "23.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = [
     (pkgs.nerdfonts.override {fonts = ["NerdFontsSymbolsOnly"];})
     pkgs-unstable.obsidian
@@ -72,30 +61,6 @@
   ];
   fonts.fontconfig.enable = true;
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #r  org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # You can also manage environment variables but you will have to manually
-  #rsource
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/jonny/etc/profile.d/hm-session-vars.sh
-  #
   programs = {
     fish.enable = true;
     git = {

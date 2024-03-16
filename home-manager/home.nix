400fce51d27e92fd6142752e74dad5cb805f2c6a
Jonny Irwin <git@jbi.im>
2024-03-16 22:05:25 +0000

    started on customising starship prompt

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,42 +1,28 @@
 {
   config,
   pkgs,
+  lib,
   pkgs-unstable,
-	nix-colors,
+  nix-colors,
   ...
 }: {
   imports = [
-		./desktop.nix
+    ./desktop.nix
     ./kitty.nix
     ./gtk.nix
     ./nvim
   ];
 
   home = {
-		username = "jonny";
-		homeDirectory = "/home/jonny";
-		sessionVariables = {
-			GPG_TTY = "$(tty)";
-			GTK_THEME = "Catppuccin-Mocha-Blue-Compact-Dark";
-			WLR_NO_HARDWARE_CURSORS = 1;
-		};
-	};
+    username = "jonny";
+    homeDirectory = "/home/jonny";
+    sessionVariables = {
+      GPG_TTY = "$(tty)";
+      GTK_THEME = "Catppuccin-Mocha-Blue-Compact-Dark";
+      WLR_NO_HARDWARE_CURSORS = 1;
+    };
+  };
   nixpkgs.config.allowUnfree = true;
-
-  #nixpkgs.overlays = [
-    #(_: prev: {
-      #aseprite = prev.aseprite.overrideAttrs (_: _: rec {
-        #version = "1.3.5";
-        #src = prev.fetchFromGitHub {
-          #owner = "aseprite";
-          #repo = "aseprite";
-          #rev = "v${version}";
-          #fetchSubmodules = true;
-          #hash = "sha256-QXhoSfVyMLLTKFUDeX7WaNcX2IvDK729LxV0u0q1AoA=";
-        #};
-      #});
-    #})
-  #];
 
   home.stateVersion = "23.05"; # Please read the comment before changing.
 
@@ -44,53 +30,51 @@
     (pkgs.nerdfonts.override {fonts = ["NerdFontsSymbolsOnly"];})
     pkgs-unstable.obsidian
     pkgs.keepassxc
-		pkgs.cowsay
+    pkgs.cowsay
     pkgs.intel-one-mono
     pkgs.waybar
     pkgs.inter
-		pkgs.lolcat
+    pkgs.lolcat
     pkgs-unstable.godot_4
-    pkgs.starship
     pkgs-unstable.aseprite
     pkgs.python3
     pkgs.qutebrowser
     pkgs.microsoft-edge
     pkgs.nodejs
-		pkgs.spotify
-		pkgs.cabal-install
-		pkgs.ghc
-		pkgs.meld
-		pkgs.neofetch
-		pkgs-unstable.discord
-		pkgs.libnotify
-		pkgs.glib
-		pkgs.unzip
-		pkgs.elixir
+    pkgs.spotify
+    pkgs.cabal-install
+    pkgs.ghc
+    pkgs.meld
+    pkgs.neofetch
+    pkgs-unstable.discord
+    pkgs.libnotify
+    pkgs.glib
+    pkgs.unzip
+    pkgs.elixir
   ];
   fonts.fontconfig.enable = true;
 
-	colorScheme = nix-colors.colorSchemes.catppuccin-frappe;
+  colorScheme = nix-colors.colorSchemes.catppuccin-frappe;
 
-services = {
+  services = {
+    syncthing.enable = true;
 
-	syncthing.enable = true;
+    gpg-agent = {
+      enable = true;
+      enableSshSupport = true;
+      pinentryFlavor = "tty";
+    };
 
-  gpg-agent = {
-    enable = true;
-    enableSshSupport = true;
-    pinentryFlavor = "tty";
+    network-manager-applet.enable = true;
   };
-
-  network-manager-applet.enable = true;
-};
 
   programs = {
     fish = {
-			enable = true;
-			interactiveShellInit = ''
-				set fish_greeting
-			'';	
-		};
+      enable = true;
+      interactiveShellInit = ''
+        set fish_greeting
+      '';
+    };
     git = {
       enable = true;
       userEmail = "git@jbi.im";
@@ -110,7 +94,72 @@ services = {
     };
     gpg.enable = true;
     home-manager.enable = true;
-    starship.enable = true;
+    starship = let
+      inherit (config.colorScheme.palette) base00 base01 base02 base03 base04 base05 base06 base07 base08 base09 base0A base0B base0C base0D base0E base0F;
+    in {
+      enable = true;
+      enableFishIntegration = true;
+      #enableTransience = true;
+      settings = {
+        add_newline = true;
+        #palette = "base16";
+
+        format = lib.concatStrings [
+          "[](fg:#${base0A})"
+          "$username"
+          "[ ](fg:#${base0A} bg:#${base0D})"
+					"$directory"
+          "[](fg:#${base0D})"
+          "[](fg:#${base02})"
+          "[](fg:#${base03})"
+          "[](fg:#${base04})"
+          "[](fg:#${base05})"
+          "[](fg:#${base06})"
+          "[](fg:#${base07})"
+          "[](fg:#${base08})"
+          "[](fg:#${base09})"
+          "[](fg:#${base0A})"
+          "[](fg:#${base0B})"
+          "[](fg:#${base0C})"
+          "[](fg:#${base0D})"
+          "[](fg:#${base0E})"
+          "[](fg:#${base0F})"
+          "$line_break"
+          "$character"
+        ];
+
+        username = {
+          style_user = "bold fg:#${base00} bg:#${base0A}";
+          style_root = "";
+          show_always = true;
+          format = "[$user]($style)";
+        };
+
+				directory = {
+					style = "fg:#${base02} bg:#${base0D}";
+					format = "[$path]($style)";
+				};
+
+        #palettes.base16 = {
+        #base00 = "#${config.colorScheme.palette.base00}";
+        #base01 = "#${config.colorScheme.palette.base01}";
+        #base02 = "#${config.colorScheme.palette.base02}";
+        #base03 = "#${config.colorScheme.palette.base03}";
+        #base04 = "#${config.colorScheme.palette.base04}";
+        #base05 = "#${config.colorScheme.palette.base05}";
+        #base06 = "#${config.colorScheme.palette.base06}";
+        #base07 = "#${config.colorScheme.palette.base07}";
+        #base08 = "#${config.colorScheme.palette.base08}";
+        #base09 = "#${config.colorScheme.palette.base09}";
+        #base0A = "#${config.colorScheme.palette.base0A}";
+        #base0B = "#${config.colorScheme.palette.base0B}";
+        #base0C = "#${config.colorScheme.palette.base0C}";
+        #base0D = "#${config.colorScheme.palette.base0D}";
+        #base0E = "#${config.colorScheme.palette.base0E}";
+        #base0F = "#${config.colorScheme.palette.base0F}";
+        #};
+      };
+    };
     ripgrep.enable = true;
     fzf = {
       enable = true;
@@ -124,5 +173,5 @@ services = {
     shortcut = "a";
   };
 
-	environment.desktop = "sway";
+  environment.desktop = "sway";
 }

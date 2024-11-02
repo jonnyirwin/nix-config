a470717bf2a5705f6f8ac2e0ca9aad7f2dfe9e9e
Jonny Irwin <git@jbi.im>
2024-11-02 22:17:26 +0000

    updates

--- home-manager/wm/sway.nix
+++ home-manager/wm/sway.nix
@@ -90,7 +90,7 @@
           "${modifier}+Shift+l" = "exec swaylock -f -c 000000";
 					"XF86MonBrightnessUp" = "exec brightnessctl set 5%+";
 					"XF86MonBrightnessDown" = "exec brightnessctl set 5%-";
-					"XF86AudioRaiseVolume" = "exec wpctlset-volume @DEFAULT_SINK@ .05+";
+					"XF86AudioRaiseVolume" = "exec wpctl set-volume @DEFAULT_SINK@ .05+";
 					"XF86AudioLowerVolume" = "exec wpctl set-volume @DEFAULT_SINK@ .05-";
 					"XF86AudioMute" = "exec wpctl set-mute @DEFAULT_SINK@ toggle";
 					"${modifier}+p" = "exec grimshot save active";
